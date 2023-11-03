0591a35cf202ab80f866aaeeb27b8c5e9aced752
dongshu2013 <dongs2011@gmail.com>
2023-11-03 14:56:30 -0700

    remove useless code

--- src/utils.ts
+++ src/utils.ts
@@ -9,19 +9,6 @@ export const epoch = () => {
   return Math.floor(new Date().getTime() / 1000);
 };
 
-export const toEthSignedMessageHash = function(messageHex: string) {
-  return ethers.keccak256(
-      ethers.solidityPacked(["string", "bytes32"],
-          ["\x19Ethereum Signed Message:\n32", messageHex]));
-};
-
-export const DAUTH_JWK = {
-  "kty": "EC",
-  "crv": "P-256",
-  "x": "G1kL6UJ85jAXwC40y-YFdjn4PEluaRvWQQYG1sDcNao",
-  "y": "a66lvKe4rCiwbQUxqjFZT8R-0xdGEJgeGXtNC9XclaY",
-};
-
 export class HexlinkError extends Error {
   code: number;
 
@@ -29,17 +16,6 @@ export class HexlinkError extends Error {
     super(message);
     this.code = code;
   }
-}
-
-export function getAuthToken(req: functions.https.Request) {
-  const authZ = req.get("Authorization");
-  if (authZ) {
-    const tokens = authZ.split("Bearer ");
-    if (tokens.length > 1) {
-      return tokens[1];
-    }
-  }
-  return req.body.authToken;
 }
 
 export const handleError = function(
@@ -52,18 +28,3 @@ export const handleError = function(
   console.log("Error: ", err);
   res.status(500).json({message: "internal server error"});
 };
-
-export function normalizeEmail(email: string) {
-  return email.trim().toLowerCase();
-}
-
-export function emailHash(email: string) {
-  const normalized = normalizeEmail(email);
-  return hash(`mailto:${normalized}`);
-}
-
-export function maskEmail(email: string) {
-  const normalized = normalizeEmail(email);
-  const [name, domain] = normalized.split("@");
-  return `${name[0]}***@${domain}`;
-}
