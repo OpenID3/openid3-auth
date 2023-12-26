500c5f79ad881aca3272e1ad86e88e586906c615
dongshu2013 <dongs2011@gmail.com>
2023-12-26 14:05:36 -0800

    use REACT_APP_ORIGIN over ORIGIN

--- src/auth.ts
+++ src/auth.ts
@@ -187,7 +187,7 @@ export const loginWithPasskey = functions.https.onRequest((req, res) => {
           req.body.clientDataJson,
           [
             ["challenge", challenge],
-            ["origin", secrets.ORIGIN],
+            ["origin", secrets.REACT_APP_ORIGIN],
           ],
           req.body.authData,
           req.body.signature,
