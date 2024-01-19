b12e9427f9d6967bdb8d8ca01fe2d313342481a3
pette1999 <peterchen.eth@gmail.com>
2024-01-20 00:27:49 +0400

    fix script directory

--- scripts/server.sh
+++ scripts/server.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd $SCRIPT_DIR/../wellknown
+cd $SCRIPT_DIR/../indexer
 
 echo "wellknown: installing dependencies..."
 pnpm i
